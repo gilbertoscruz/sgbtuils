5feb91fec20667133155d956504d8c88cbc7a157
Gilberto Cruz <gilbertoscruz@gmail.com>
2017-01-26 23:43:56 -0200

    Update Consts.java

--- library/src/main/java/br/com/sgb/sgbutil/consts/Consts.java
+++ library/src/main/java/br/com/sgb/sgbutil/consts/Consts.java
@@ -10,5 +10,6 @@ package br.com.sgb.sgbutil.consts;
 public class Consts {
 
 	public static final String TAG = "SGBUtils";
+	public static final String URL_PING_INTERNET = "http://www.google.com";
 	
 }
